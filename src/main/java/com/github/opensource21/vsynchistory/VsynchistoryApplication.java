a3946be2e01aee59e853ec714ac716e6d87f1df6
niels <opensource21@gmail.com>
2015-09-25 12:07:29 +0200

    Create a spring boot project.

---
+++ src/main/java/com/github/opensource21/vsynchistory/VsynchistoryApplication.java
@@ -0,0 +1,12 @@
+package com.github.opensource21.vsynchistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsynchistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VsynchistoryApplication.class, args);
+    }
+}
