aab6eb102dffd086a4f7d83ea1576d6cbbaccc0d
niels <opensource21@gmail.com>
2016-05-01 20:25:59 +0200

    Das Diffen und Commiten in einem Service konzentriert.

---
+++ src/main/java/com/github/opensource21/vsynchistory/service/api/DiffService.java
@@ -0,0 +1,33 @@
+/**
+ *
+ */
+package com.github.opensource21.vsynchistory.service.api;
+
+import java.io.IOException;
+import java.util.Set;
+
+import net.fortuna.ical4j.data.ParserException;
+
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+/**
+ * Service which compares the caldendar and contacts with the repository state.
+ * @author niels
+ *
+ */
+public interface DiffService {
+
+	/**
+	 * Commit current changes.
+	 * @param additionalCommitMessage an additional message for the commit.
+	 * @param changedFilenames list of file which should be compared.
+	 * @throws GitAPIException
+	 * @throws IOException
+	 * @throws ParserException
+	 */
+	void commitChanges(String additionalCommitMessage, Set<String> changedFilenames) throws GitAPIException,
+			IOException, ParserException;
+
+
+
+}
---
+++ src/main/java/com/github/opensource21/vsynchistory/service/impl/DiffServiceImpl.java
@@ -0,0 +1,99 @@
+/**
+ *
+ */
+package com.github.opensource21.vsynchistory.service.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+
+import javax.annotation.Resource;
+
+import net.fortuna.ical4j.data.ParserException;
+
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.github.opensource21.vsynchistory.controller.CommandlineController;
+import com.github.opensource21.vsynchistory.model.DiffResult;
+import com.github.opensource21.vsynchistory.service.api.AddressService;
+import com.github.opensource21.vsynchistory.service.api.CalendarService;
+import com.github.opensource21.vsynchistory.service.api.DiffService;
+import com.github.opensource21.vsynchistory.service.api.GitService;
+import com.github.opensource21.vsynchistory.service.api.GitService.ContentTupel;
+
+/**
+ * Default-Implementierung of {@link DiffService}.
+ *
+ * @author niels
+ *
+ */
+@Service
+public class DiffServiceImpl implements DiffService {
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(CommandlineController.class);
+
+	private static final String PROPS_FILESUFFIX = ".props";
+	private static final String CALENDAR = "BEGIN:VCALENDAR";
+	private static final String ADDRESS = "BEGIN:VCARD";
+
+	@Resource
+	private GitService gitService;
+
+	@Resource
+	private CalendarService calendarService;
+
+	@Resource
+	private AddressService addressService;
+
+	@Override
+	public void commitChanges(String additionalCommitMessage, final Set<String> changedFilenames)
+			throws GitAPIException, IOException, ParserException {
+		for (final String changedFile : changedFilenames) {
+			final ContentTupel changes = gitService.getContents(changedFile);
+			final InputStream oldContent = changes.getOldContent();
+			final byte[] typeAsBytes = new byte[CALENDAR.length()];
+			oldContent.read(typeAsBytes);
+			final String type = new String(typeAsBytes);
+			oldContent.reset();
+			final DiffResult diffResult;
+			if (CALENDAR.equals(type)) {
+				gitService.add(changedFile, changedFile + PROPS_FILESUFFIX);
+				diffResult = calendarService.compare(oldContent,
+						changes.getNewContent());
+			} else if (type.startsWith(ADDRESS)) {
+				gitService.add(changedFile, changedFile + PROPS_FILESUFFIX);
+				diffResult = addressService.compare(oldContent,
+						changes.getNewContent());
+			} else {
+				diffResult = null;
+			}
+
+			if (diffResult == null) {
+				if (!changedFile.endsWith(PROPS_FILESUFFIX)) {
+					LOG.error("Unhandled file {}.", changedFile);
+				}
+			} else {
+				final StringBuilder commitMessage = new StringBuilder(
+						additionalCommitMessage);
+				commitMessage.append(changedFile).append(" :");
+				commitMessage.append(" -").append(
+						diffResult.getNrOfDeletedEntries());
+				commitMessage.append(" ~").append(
+						diffResult.getNrOfChangedEntries());
+				commitMessage.append(" +").append(
+						diffResult.getNrOfNewEntries());
+				if (diffResult.getTotalChanges() > 10) {
+					LOG.error("Many changes: {}", commitMessage);
+				}
+				commitMessage.append("\n");
+				commitMessage.append(diffResult.getCommitMessage());
+				gitService.commit(commitMessage.toString(), changedFile,
+						changedFile + PROPS_FILESUFFIX);
+			}
+		}
+	}
+}
