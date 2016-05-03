4a15d3621c20a5127d39df39e98640a8f060c118
niels <opensource21@gmail.com>
2016-05-03 17:42:23 +0200

    Log ergänzt

--- src/main/java/com/github/opensource21/vsynchistory/trigger/FastCommit.java
+++ src/main/java/com/github/opensource21/vsynchistory/trigger/FastCommit.java
@@ -36,68 +36,74 @@ import com.github.opensource21.vsynchistory.service.api.GitService;
  *
  */
 @Component
-public class FastCommit implements InitializingBean  {
+public class FastCommit implements InitializingBean {
 
-	private static Logger LOG = LoggerFactory.getLogger(DailyCommit.class);
+    private static Logger LOG = LoggerFactory.getLogger(DailyCommit.class);
 
-	@Resource
-	private GitService gitService;
+    @Resource
+    private GitService gitService;
 
-	@Resource
-	private CalendarService calendarService;
+    @Resource
+    private CalendarService calendarService;
 
-	@Resource
-	private DiffService diffService;
+    @Resource
+    private DiffService diffService;
 
-	@Value(value = "${repositoryLocation}")
-	private String repositoryLocation;
+    @Value(value = "${repositoryLocation}")
+    private String repositoryLocation;
 
-	private WatchService watcher;
+    private WatchService watcher;
 
-	private volatile long lastChange = Long.MAX_VALUE;
+    private volatile long lastChange = Long.MAX_VALUE;
 
-	@Value("${watch.silencetime}")
-	private long silenceTime;
+    @Value("${watch.silencetime}")
+    private long silenceTime;
 
-	@Scheduled(initialDelay = 1500, fixedDelayString = "${watch.fixedDelay}")
+    @Scheduled(initialDelay = 1500, fixedDelayString = "${watch.fixedDelay}")
     public void dailyCommit() throws Exception {
-		if ((System.currentTimeMillis() - lastChange) > silenceTime) {
-			LOG.info("Run-Commit");
-			diffService.commitChanges("", gitService.getChangedFilenames());
-			lastChange = Long.MAX_VALUE;
-		}
+
+        final long currentTimeMillis = System.currentTimeMillis();
+        final long timeSinceLastChange = currentTimeMillis - lastChange;
+        LOG.debug(
+                "Currenttime: {}, lastChange{}, timeSinceLastChange: {}, silenceTime: {}",
+                currentTimeMillis, lastChange, timeSinceLastChange, silenceTime);
+        if (timeSinceLastChange > silenceTime) {
+            LOG.info("Run-Commit");
+            diffService.commitChanges("", gitService.getChangedFilenames());
+            lastChange = Long.MAX_VALUE;
+        }
     }
 
-	@Scheduled(initialDelay = 1000, fixedDelayString = "${watch.fixedDelay}")
-	public void waitForChanges() {
-		try {
-			final WatchKey watckKey = watcher.take();
-			final List<WatchEvent<?>> events = watckKey.pollEvents();
-			for (final WatchEvent<?> event : events) {
-				if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
-					LOG.debug("Created: {} ", event.context());
-				}
-				if (event.kind() == StandardWatchEventKinds.ENTRY_DELETE) {
-					LOG.debug("Delete: {}", event.context());
-				}
-				if (event.kind() == StandardWatchEventKinds.ENTRY_MODIFY) {
-					LOG.debug("Modify: {}", event.context());
-				}
-			}
-			lastChange = System.currentTimeMillis();
-			watckKey.reset();
-		} catch (final InterruptedException e) {
-			LOG.error("Filewatch wurd unterbrochen: ", e);
-		}
-	}
+    @Scheduled(initialDelay = 1000, fixedDelayString = "${watch.fixedDelay}")
+    public void waitForChanges() {
+        try {
+            final WatchKey watchKey = watcher.take();
+            final List<WatchEvent<?>> events = watchKey.pollEvents();
+            for (final WatchEvent<?> event : events) {
+                if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
+                    LOG.debug("Created: {} ", event.context());
+                }
+                if (event.kind() == StandardWatchEventKinds.ENTRY_DELETE) {
+                    LOG.debug("Delete: {}", event.context());
+                }
+                if (event.kind() == StandardWatchEventKinds.ENTRY_MODIFY) {
+                    LOG.debug("Modify: {}", event.context());
+                }
+            }
+            lastChange = System.currentTimeMillis();
+            watchKey.reset();
+        } catch (final InterruptedException e) {
+            LOG.error("Filewatch wurd unterbrochen: ", e);
+        }
+    }
 
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		final Path repoDir = Paths.get(repositoryLocation);
-		watcher = repoDir.getFileSystem().newWatchService();
-		registerAll(repoDir);
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        final Path repoDir = Paths.get(repositoryLocation);
+        watcher = repoDir.getFileSystem().newWatchService();
+        registerAll(repoDir);
 
-	}
+    }
 
     /**
      * Register the given directory, and all its sub-directories, with the
@@ -107,12 +113,11 @@ public class FastCommit implements InitializingBean  {
         // register directory and sub-directories
         Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
             @Override
-            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
-                throws IOException
-            {
-            	dir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE,
-        				StandardWatchEventKinds.ENTRY_DELETE,
-        				StandardWatchEventKinds.ENTRY_MODIFY);
+            public FileVisitResult preVisitDirectory(Path dir,
+                    BasicFileAttributes attrs) throws IOException {
+                dir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE,
+                        StandardWatchEventKinds.ENTRY_DELETE,
+                        StandardWatchEventKinds.ENTRY_MODIFY);
                 return FileVisitResult.CONTINUE;
             }
         });
